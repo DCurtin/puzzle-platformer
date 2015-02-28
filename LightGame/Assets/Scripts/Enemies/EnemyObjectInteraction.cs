d4275ce38f5361edffeae706b5ab2c788f32ca99
Metiri <ramon_meza@live.com>
2015-02-28 10:43:15 -0500

    Base Project
    This is the inital project. I have saved a local copy as well, for
    backup purposes.

---
+++ LightGame/Assets/Scripts/Enemies/EnemyObjectInteraction.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyObjectInteraction : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
