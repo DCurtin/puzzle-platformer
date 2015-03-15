6b7809b6fc753b48bc6ce0e57bf8e8acea4a6bf9
DCurtin <daltoncurtin@gmail.com>
2015-03-15 12:46:23 -0400

    Follow Script
    Cleaned up the follow script a bit, added a use Original Position bool
    to control how you would like the object to be position on game start

--- LightGame/Assets/Scripts/Misc/FlashlightRay.cs
+++ LightGame/Assets/Scripts/Misc/FlashlightRay.cs
@@ -10,34 +10,84 @@ using System.Collections;
 
 public class FlashlightRay : MonoBehaviour {
 
+	public int reflections = 3;
 	public float rayLength = 13f;
+
+	private Ray2D ray;
+	private GameObject target;
+
+	//any scripts that will be controlled by this ray should be declared here
+	//this script controls light trigger game objects
+	private LightTrigger targetController;
 
 	void Update()
 	{
-		//this line instantiates a Raycast hit simmilar to a collider2D that gives us info
-		//about what the ray is hitting. The raycast itself takes in a position, a direction and
-		//distance and returns the RaycastHit when it interacts with something
-		RaycastHit2D hitinfo = Physics2D.Raycast(transform.position, transform.right, rayLength);
+		//get a gameobject from the castRay function
+		target = castRay (transform.position, transform.right, rayLength, 0);
 
-		//here we get a collider from hitinfo. When the ray is not hitting anything it's 
-		//return type is null so hitinfo will be null
-		if (hitinfo.collider != null) 
+		//if the target is not null
+		if (target) 
 		{
-			//here we get the game object's name and compare it to a Ligh trigger
-			if(hitinfo.collider.gameObject.name == "Light Trigger")
-			{
-				//here we preform the approriate action for this object
-				//which is running a set of methods after getting the script from the
-				//light trigger object
-				LightTrigger lightScript = hitinfo.collider.GetComponent<LightTrigger>();
-				lightScript.setStateTrue();
-				lightScript.setStateFalse();
-				
-
+			//create an if statement for any object that will be affect by the light ray
+			//this if statment will check for gameobjects named "Light Trigger"
+			if (target.name == "Light Trigger") {
+				targetController = target.GetComponent<LightTrigger> ();
+				targetController.setStateTrue ();
 			}
 		}
+	}
+
+	//give an origin and direction
+	//cast from that origin
+	//return gameObject
+	GameObject castRay(Vector2 origin, Vector2 direction, float length, int reflected)
+	{
 		
+		//here we get a collider from hitinfo. When the ray is not hitting anything it's 
+		//return type is null so hitinfo will be null
+
+
+		//here the loop is basically just spinning through quick enough to catch any reflections that may occur
+		//if none occur then it will just redraw the same ray three times. This method saves the game from crashing 
+		//since less loop cycles will occur then if we set the increment to only occur if there's an actual reflection
+		
+			while(reflected <= reflections)
+			{
+				//if the ray is hitting a gameobject (the ray is not returning null)
+				if (Physics2D.Raycast(origin, direction, rayLength)) 
+				{
+					//define a ray
+					ray = new Ray2D(origin,direction);
+					//then cast the ray, collecting any info that may be returned
+					RaycastHit2D hitinfo = Physics2D.Raycast(ray.origin, ray.direction, rayLength);
+
+					//draw the beam	
+					//Debug.DrawRay(ray.origin, ray.direction * 50, Color.cyan);
+
+
+					//if the ray hits a mirror, reflect
+					if(hitinfo.collider.gameObject.name == "Mirror")
+					{
+						//a reflection is caused by redefining the origin and direction of the
+						//beam
+						direction = Vector3.Reflect(ray.direction, hitinfo.normal); 
+						origin = hitinfo.point;
+						
+					//otherwise return whatever was hit
+					}else
+						return hitinfo.collider.gameObject;
+					
+				//if nothing was hit increment reflected regardless (to avoid crashing)
+				//and recast another ray
+				}
+				reflected++;
+			}
+			return null;
+								
+		}
+
 
 	}
 
-}
+
+
--- LightGame/Assets/Scripts/Misc/FollowPlayer.cs
+++ LightGame/Assets/Scripts/Misc/FollowPlayer.cs
@@ -8,6 +8,8 @@ public class FollowPlayer : MonoBehaviour {
 	public bool followW = true;
 	public bool followH = true;
 
+	public bool useOriginalPosition = true;
+
 	private float w = 0, h = 0;
 
 	public float height = 0f;
@@ -16,22 +18,44 @@ public class FollowPlayer : MonoBehaviour {
 
 
 	private Vector3 offset;
+	private Vector3 originalPos;
 	
 	// Use this for initialization
 	void Start () 
 	{
-		if (followH)
-			h = 1;
-		if (followW)
-			w = 1;
-	 
-		offset = new Vector3( transform.position.x * w + width  , transform.position.y * h + height , depth);	
-		transform.position = offset;
+		//save original position and create an offset based on player 
+		originalPos = transform.position;
 	}
 
 	void Update()
 	{
-		transform.position = new Vector3(player.transform.position.x * w + width, player.transform.position.y * h + height, depth) + offset;
+		//h and w are float
+		//equivalents of folloH and folloW
+		//these values can be used to zero out
+		//play position as a modification on 
+		//certain planes
+		if (followH)
+			h = 1;
+		else
+			h = 0;
+
+		if (followW)
+			w = 1;
+		else
+			w = 0;
+
+		//when not using the original position
+		//the object will snap to the player's position and will be offset by the values input
+		//note: the object will only snap to what is enabled via followH and followW
+		if (!useOriginalPosition) {
+			transform.position = player.transform.position;
+			transform.position = new Vector3 (player.transform.position.x * w + width, player.transform.position.y * h + height, depth);
+		} else {
+			//when using the original position we neglect the width and height floats and only use the objects original position 
+			//and using player position to update it's position
+			//width and height may be removed later if this method is sufficient 
+			transform.position = new Vector3 (player.transform.position.x * w + originalPos.x, player.transform.position.y * h + originalPos.y, depth);
+		}
 	}
 	
 	// Update is called once per frame
